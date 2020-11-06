c54d1cfe0e801825f2d83180fea8c0135c627a5a
Raj <raju.11811924@lpu.in>
2020-11-06 14:40:40 +0530

    Installed required libraries

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hit List</h1>
+      </>
+    );
+  }
+}
+
+export default App;
